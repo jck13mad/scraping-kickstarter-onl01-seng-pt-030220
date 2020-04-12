663a284915e9bbf12c8ee278d5866dac951339b6
Jack Madden <jck13mad@gmail.com>
2020-04-12 23:21:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,6 @@ require "open-uri"
 def create_project_hash
   
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
